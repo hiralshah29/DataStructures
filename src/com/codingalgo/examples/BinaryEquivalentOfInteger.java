b3ac6497afbf2af606d9040fe8e9a086f6bb9055
hiralshah29 <hiral.shah.29@gmail.com>
2017-06-18 11:40:57 -0700

    Adding new file. To find binary equivalent of Integer.

---
+++ src/com/codingalgo/examples/BinaryEquivalentOfInteger.java
@@ -0,0 +1,25 @@
+package com.codingalgo.examples;
+
+public class BinaryEquivalentOfInteger {
+	
+	public static void main(String[] args) {
+		new BinaryEquivalentOfInteger().binaryEquiv(955);
+	}
+	
+	public void binaryEquiv(int n){
+		
+		int[] d = new int[30];
+		int l = 0 ;
+		int p;
+		
+		while (n > 0){
+			d[l]=n%2;
+			n = n/2;
+			l++;
+		}
+		
+		for(int i = l-1 ; i >= 0 ; i--){
+		       System.out.print(d[i]);
+		}
+	}
+}
